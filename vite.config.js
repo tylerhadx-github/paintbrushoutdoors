efc5e17895f6130b2c5dd8313521a0c50d6c630a
Tyler Haddix <thaddix@raptortech.com>
2023-09-06 14:19:51 -0600

    repo

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/paintbrushoutdoors/',
   plugins: [
     vue({ 
       template: { transformAssetUrls }
